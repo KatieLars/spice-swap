e9035e2dda526f5d37c85a495de034480112bc78
Katie <katmae83@gmail.com>
2019-02-15 23:27:58 -0600

    home screen background and title

--- SpiceSwap/App.js
+++ SpiceSwap/App.js
@@ -8,43 +8,43 @@
  */
 
 import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
-
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
-  android:
-    'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
-
-type Props = {};
-export default class App extends Component<Props> {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>Hello World</Text>
-        <Text style={styles.instructions}>To get started, edit App.js</Text>
-        <Text style={styles.instructions}>{instructions}</Text>
-      </View>
-    );
-  }
-}
+import {Text, View, ImageBackground, AppRegistry, StyleSheet} from 'react-native';
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
+  titleCard: {
+    color: 'white',
+    fontWeight: 'bold',
+    fontSize: 75,
+    textAlign: 'center'
   },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+   backgroundImageStyle: {
+      justifyContent: 'center',
+      alignItems: 'center',
+      width: '100%',
+      height: '100%'
+    },
+
 });
+
+
+export default class App extends Component {
+
+      render() {
+        let backgroundPic = {
+            uri: 'https://images.unsplash.com/photo-1528615798279-02f8efa6c100?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2470&q=80'
+        };
+
+        return (
+          <View style={styles} >
+            <ImageBackground source={backgroundPic} style={styles.backgroundImageStyle}>
+                <Text style={styles.titleCard}>Spice Swap</Text>
+            </ImageBackground>
+          </View>
+        );
+
+       }
+}
+//
+//AppRegistry.registerComponent('SpiceSwap', () => App)
+
+
